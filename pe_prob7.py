ad1c09dee0d9ec6376806edc3c66a40860aafaaa
Greg Schafer <grschafer@gmail.com>
2011-12-19 13:02:25 -0700

    Solved problem 7

--- pe_prob7.py
+++ pe_prob7.py
@@ -1,21 +1,38 @@
 # By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
-# What is the 10 001st prime number?
+# What is the 10,001st prime number?
 
+# easier solution: http://www.wolframalpha.com/input/?i=10001st+prime
 
-# DO NAIVE/BRUTE FORCE OR PICK A WIKIPEDIA ALG TO IMPLEMENT?
-
-# sum is (n * (n + 1) / 2), then square it
-def sq_of_sum(n):
-    return (n * (n + 1) / 2)**2
-
-def sum_of_sq(n):
-    sum = 0
-    for i in range(n + 1):
-        sum += i*i
-    return sum
+def nth_prime(n):
+    # edge case so I can increment by 2 and
+    # avoid checking primality of even numbers
+    if n == 1:
+        return 2
+    
+    
+    i = 1
+    candidate = 1
+    # store primes in a list for dividing later prime candidates
+    primes = [2]
+    
+    # while we're not at nth prime
+    while i < n:
+        candidate += 2
+        b = 0
+        
+        # loop through list of primes to see if they're factors of candidate
+        while b < len(primes) and candidate % primes[b] != 0:
+            b += 1
+        
+        # if no factor was found, candidate is a prime number
+        if b == len(primes):
+            primes.append(candidate)
+            i += 1
+    
+    return candidate
 
 def main():
-    print sq_of_sum(100) - sum_of_sq(100)
+    print(nth_prime(10001))
         
 if __name__=='__main__':
     main()
